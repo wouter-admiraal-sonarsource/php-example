cda17a0ad9f46a482c3a1680660268aad306ff7e
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2023-07-14 11:45:56 +0200

    Finish updating tests and composer autoload

--- src/main/EnvGreeter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App;
-
-class EnvGreeter extends Greeter {
-	public function greetFromEnv(string $environmentVariableName):string {
-		$name = getenv($environmentVariableName);
-		return $this->greet($name);
-	}
-}
