cf968e941a1986ba280c7d5c228895c007abe07e
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2023-07-14 12:01:58 +0200

    Update formatting

--- src/main/Greeter.php
+++ src/main/Greeter.php
@@ -2,11 +2,11 @@
 namespace App;
 
 class Greeter {
-	public function greet(string $name = null):string {
-		if($name) {
-			return "Hello, $name!";
-		}
+  public function greet(string $name = null): string {
+    if ($name) {
+      return "Hello, $name!";
+    }
 
-		return "Hello!";
-	}
+    return "Hello!";
+  }
 }
--- src/test/GreeterTest.php
+++ src/test/GreeterTest.php
@@ -4,24 +4,24 @@ namespace App\Test;
 use App\Greeter;
 
 class GreeterTest extends \PHPUnit\Framework\TestCase {
-	public function testGreeterSaysHello() {
-		$greeter = new Greeter();
-		self::assertStringContainsString(
-			"Hello",
-			$greeter->greet()
-		);
-	}
+  public function testGreeterSaysHello() {
+    $greeter = new Greeter();
+    self::assertStringContainsString(
+      "Hello",
+      $greeter->greet()
+    );
+  }
 
-	public function testGreeterUsesName() {
-		$greeter = new Greeter();
+  public function testGreeterUsesName() {
+    $greeter = new Greeter();
 
-		self::assertStringContainsString(
-			"Hello, Cody",
-			$greeter->greet("Cody")
-		);
-		self::assertStringContainsString(
-			"Hello, Sarah",
-			$greeter->greet("Sarah")
-		);
-	}
+    self::assertStringContainsString(
+      "Hello, Cody",
+      $greeter->greet("Cody")
+    );
+    self::assertStringContainsString(
+      "Hello, Sarah",
+      $greeter->greet("Sarah")
+    );
+  }
 }
